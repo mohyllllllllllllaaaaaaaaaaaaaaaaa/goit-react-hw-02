da375c106b30e04fe95339f6a20ec840fe6b509d
natalia mohyla <mogilesha81@gmail.com>
2025-03-25 21:11:39 +0100

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import './App.css';
+import  './App.css';
 import Feedback from './components/Feedback';
 import Options from './components/Options';
 import Notification from './components/Notifications';
